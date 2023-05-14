26efe6b34abdc97619150430b8f7b489e354fdcd
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-05-14 15:07:58 +0300

    Understood Data Classed

---
+++ src/main/kotlin/07.4-data_class.kt
@@ -0,0 +1,23 @@
+// normalde siniflarin varsayilan olarak to string equals gibi metotlari yoktur
+// ancak data sinifi verileri barindirarak bu siniflari otomatik olarak kullanilmasina izin verir
+data class Veriler(var veri:String){
+
+}
+
+// bu sinifi icin print nesne durumunda nesenin referansını yazar
+// ayrica equels kisminda ayni degildirler
+class VerilerNormal(var veri:String){
+
+}
+
+fun main() {
+    var veriler:Veriler = Veriler("suleyman")
+    // nesne print icerisinde yazilinca tostring varmis gibi davranir ve fieldlari yazdirir
+    println(veriler)
+
+    // direkt olarak copy ile nesneyi kopyalarak yeni nesne olusturabilir
+    var wong = veriler.copy("wong")
+    print(wong)
+
+    // bu ozellikler normal sinifta kullanilmaz data sinifina ozgudur
+}
